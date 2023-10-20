afd457b54398a2f68313a5253328cf0a37981fb0
hy <hooiyan.hoe@atoz-software.tech>
2023-10-20 20:48:39 +0800

    feat: 🏗️ change parcel to vite

--- Udemy_BuildPolymorphicComponentsWithReactTypescript/src/main.ts
+++ Udemy_BuildPolymorphicComponentsWithReactTypescript/src/main.ts
@@ -7,3 +7,13 @@ const echo = <T>(value: T): T => {
 echo(1);
 echo("Hooi Yan");
 const result = echo({});
+
+const echoLength = <T extends { length: number }>(value: T): number => {
+  console.log(value.length);
+
+  return value.length;
+};
+
+echoLength("string");
+echoLength(["aaa"]);
+echoLength(4);
---
+++ Udemy_PolymorphicReactTypescript/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Text } from "./components/Text";
+
+function App() {
+  return (
+    <>
+      <Text as="h1">Hello React + TypeScript</Text>
+
+      <Text as="h2" href="https://google.com">
+        Start editing to see some magic happen!
+      </Text>
+
+      <Text as="a" href="https://google.com">
+        Hello World
+      </Text>
+
+      <Text emmanual="haha">This is a text node with no as</Text>
+    </>
+  );
+}
+
+export default App;
