10873094e694c2dc2c76d4c83137757cc46924f7
Hooi Yan <hooiyan.hoe@atoz-software.tech>
2023-12-17 19:57:10 +0800

    update

--- Udemy_PolymorphicReactTypescript/src/components/Input.tsx
+++
@@ -1,7 +0,0 @@
-interface Props extends React.ComponentPropsWithoutRef<"input"> {
-  type: "checkbox";
-}
-
-export const Input = ({ type, ...restProps }: Props) => {
-  return <input type={type} {...restProps} />;
-};
--- calculateInterest.js
+++
@@ -1,7 +0,0 @@
-function calculateInterest(principalAmt, interestRate, monthsOfPlacement) {
-  const step1 = principalAmt * (interestRate / 100);
-  const step2 = (step1 / 365) * 31;
-  return step2 * monthsOfPlacement;
-}
-
-console.log(calculateInterest(5000, 3.85, 6));
